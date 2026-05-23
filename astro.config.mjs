2364cc3a65bdd2083dcda0e0cb656f631e2f88ca
takashi-endo <shikataudone@yahoo.co.jp>
2026-05-24 00:25:14 +0900

    fix-speed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-
 import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
   site: 'https://taka-techblog.com',
@@ -9,5 +9,13 @@ export default defineConfig({
   build: {
     format: 'directory',
   },
-  integrations: [sitemap(), mdx()],
+  integrations: [
+    sitemap(),
+    mdx(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push', 'gtag'],
+      },
+    }),
+  ],
 });
